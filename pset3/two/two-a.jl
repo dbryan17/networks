f512ca0a920ff4e0448a5519be4edad517b20b35
dbryan17 <dfbryan17@gmail.com>
2025-02-25 21:14:37 -0700

    bugssss

--- pset3/two/two-a.jl
+++ pset3/two/two-a.jl
@@ -148,6 +148,12 @@ function jc(n1_neigh :: Set{Int}, n2_neigh :: Set{Int}):: Float64
   inter = intersect(n1_neigh, n2_neigh)
   un = union(n1_neigh, n2_neigh)
 
+  if (un == 0)
+    return 0.0
+  end
+
+
+
   return length(inter) / length(un)
 
 end 
@@ -162,12 +168,12 @@ function create_graph(edgeSet, length)
   for (u, v) in edgeSet
     add_edge!(g, u, v)
   end
+
   return g
 end 
 
 function shortest_path_preder(length) 
-  if length == 0
-    # print("INFFF")
+  if length == Inf
     return 0.0
   else
     return 1/length
@@ -193,9 +199,88 @@ end
 
 
 
+function sort_table(score_table, sort_by) 
+  # i j tk JC DP SP TPR FPR 
+  sorted_table = []
+  if (sort_by == "jc")
+    sorted_table = sort(score_table, by = x -> x[4], rev=true)
+
+  elseif (sort_by == "dp")
+    sorted_table = sort(score_table, by = x -> x[5], rev=true)
+
+  
+  elseif (sort_by == "sp")
+    # this one will be smallest shortest path is most likely to make edge, so not
+    sorted_table = sort(score_table, by = x -> x[6], rev=false)
+
+  else
+    print("ERRROR")
+  end
+
+  return sorted_table
+end
+
+  
+
+function calc_auc(score_table)
+  auc_sum = 0 
+  prev_fpr = 0
+  # i j tk JC DP SP TPR FPR 
+  for line in score_table
+    tpr = line[7]
+    fpr = line[8]
+    auc_sum += tpr * (fpr - prev_fpr)
+    if (prev_fpr > fpr) 
+      println("not increasing")
+      println(prev_fpr)
+      println(fpr)
+    end
+    prev_fpr = fpr
+
+  end
+
+  return auc_sum
+
+end
+
+# will not edit in place
+function calc_tpr_and_fpr(score_table, sort_by, num_true_pos)
+  # first need to sort based on the thing we want 
+  # i j tk JC DP SP TPR FPR 
+  sorted_table = sort_table(score_table, sort_by)
+
+  total_fp = length(score_table) - num_true_pos
+
+  tp_seen_so_far = 0 
+  fp_seen_so_far = 0
+  for (i, line) in enumerate(sorted_table)
+    if line[3] == 1
+      tp_seen_so_far += 1
+    else 
+      fp_seen_so_far += 1
+    end
+    tpr = tp_seen_so_far / num_true_pos
+    fpr = fp_seen_so_far / total_fp
+    sorted_table[i][7] = tpr
+    sorted_table[i][8] = fpr
+  end
+
+  if (tp_seen_so_far != num_true_pos) 
+    print("PROBLEM")
+  end
+  if (fp_seen_so_far != total_fp)
+    print("RRRR")
+  end
+
+  # now we have sorted table
 
 
-function make_predictions(edge_dict_obs :: Dict{Int, Set{Int}}, edges_obs :: Set{Tuple{Int, Int}}, edges_missing :: Set{Tuple{Int, Int}}) :: Vector{Vector{Float64}}
+  return sorted_table
+
+
+end
+
+function make_predictions(edge_dict_obs :: Dict{Int, Set{Int}}, edges_obs :: Set{Tuple{Int, Int}}, edges_missing :: Set{Tuple{Int, Int}})
   # we make a prediction for each possible missing edge
 
   # this will be for a partially observed graph
@@ -204,6 +289,7 @@ function make_predictions(edge_dict_obs :: Dict{Int, Set{Int}}, edges_obs :: Set
 
   edges_to_predict :: Set{Tuple{Int, Int}} = get_edges_to_pred(edge_dict_obs, edges_obs)
 
+  
   g = create_graph(edges_obs, length(edge_dict_obs))
 
   # keys are nodes, vertices are the weird thing that is returned form the built in shortest path alg
@@ -211,12 +297,15 @@ function make_predictions(edge_dict_obs :: Dict{Int, Set{Int}}, edges_obs :: Set
   shortest_path_from = Dict()
   for (i, j) in edges_to_predict
     if !(i in keys(shortest_path_from))
-      shortest_path_from[i] = dijkstra_shortest_paths(g, i).pathcounts
+      # print(i)
+      # println(dijkstra_shortest_paths(g, epi, [i]).dists)
+      shortest_path_from[i] = dijkstra_shortest_paths(g, i).dists
     end
     if !(j in keys(shortest_path_from))
-      shortest_path_from[j] = dijkstra_shortest_paths(g, j).pathcounts
+      shortest_path_from[j] = dijkstra_shortest_paths(g, j).dists
     end
   end
+
 
   # i j tk JC DP SP TPR FPR 
 
@@ -225,10 +314,12 @@ function make_predictions(edge_dict_obs :: Dict{Int, Set{Int}}, edges_obs :: Set
   for (i, j) in edges_to_predict
 
     # do predictions
-    jc_score = jc(edge_dict_obs[i], edge_dict_obs[j]) * (rand() * 0.0001)
-    dp_score = dp(edge_dict_obs[i], edge_dict_obs[j]) * (rand() * 0.0001)
+    jc_score = jc(edge_dict_obs[i], edge_dict_obs[j]) + (rand() * 0.00000001)
+    dp_score = dp(edge_dict_obs[i], edge_dict_obs[j]) + (rand() * 0.00000001)
+    # TODO if it is zero, need to add
     shortest_path = shortest_path_from[i][j]
-    sp_score = shortest_path_preder(shortest_path) * (rand() * 0.0001)
+    sp_score = shortest_path_preder(shortest_path) + (rand() * 0.00000001)
+    
     tau = 0
     if (i,j) in edges_missing
       tau = 1
@@ -237,8 +328,11 @@ function make_predictions(edge_dict_obs :: Dict{Int, Set{Int}}, edges_obs :: Set
   end
 
   # TODO do real calculation of TPR and FPR
+  score_table_jc = calc_tpr_and_fpr(score_table, "jc", length(edges_missing))
+  score_table_dp = calc_tpr_and_fpr(score_table, "dp", length(edges_missing))
+  score_table_sp = calc_tpr_and_fpr(score_table, "sp", length(edges_missing))
 
-  return score_table
+  return (score_table_jc, score_table_dp, score_table_sp)
 
 end
 
@@ -255,37 +349,113 @@ function two_a(edge_dict :: Dict{Int, Set{Int}}, edges :: Set{Tuple{Int, Int}})
 
   frac_to_obs = 0.05
 
+  num_rand_graphs = 1
+
+  iters_for_each_g = 1
+
+  f_to_jp_auc = []
+  f_to_dp_auc = []
+  f_to_sp_auc = []
+
   while frac_to_obs < 1
-    (edge_dict_obs, edges_obs, rmed_edges) = remove_some_edges(edge_dict, edges, frac_to_obs)
     println(frac_to_obs)
-    score_table = make_predictions(edge_dict_obs, edges_obs, rmed_edges)
+    # get get more options of removed edges
+
+    sum_jp_auc = 0 
+    sum_dp_auc = 0 
+    sum_sp_auc = 0
+    for i in 1:num_rand_graphs
+      (edge_dict_obs, edges_obs, rmed_edges) = remove_some_edges(edge_dict, edges, frac_to_obs)
+      # to get average AUC
+      for i in 1:iters_for_each_g
+        (jp_st, dp_st, sp_st) = make_predictions(edge_dict_obs, edges_obs, rmed_edges)
+        jp_auc = calc_auc(jp_st)
+        sum_jp_auc += jp_auc
+        dp_auc = calc_auc(dp_st)
+        sum_dp_auc += dp_auc
+        sp_auc = calc_auc(sp_st)
+        sum_sp_auc += sp_auc
+      end
+    end
+
+
+    avg_jp_auc = sum_jp_auc / (num_rand_graphs * iters_for_each_g)
+    avg_dp_auc = sum_dp_auc / (num_rand_graphs * iters_for_each_g)
+    avg_sp_auc = sum_sp_auc / (num_rand_graphs * iters_for_each_g)
+
+    push!(f_to_jp_auc, (frac_to_obs, avg_jp_auc))
+    push!(f_to_dp_auc, (frac_to_obs, avg_dp_auc))
+    push!(f_to_sp_auc, (frac_to_obs, avg_sp_auc))
+
     frac_to_obs += 0.05
 
+
+
   end
+
+  return (f_to_jp_auc, f_to_dp_auc, f_to_sp_auc)
 end
 
 ####### here start problem #########
 
-two_a(nbd_adj_list, nbd_edges)
-two_a(m_adj_list, m_edges)
+(nbd_f_to_jc, nbd_f_to_dp, nbd_f_to_sp) = two_a(nbd_adj_list, nbd_edges)
+(m_f_to_jc, m_f_to_dp, m_f_to_sp) = two_a(m_adj_list, m_edges)
 ####################################
 
+if (length(nbd_f_to_dp) != length(m_f_to_jc))
+  print("********")
+end
 
 
-# TODO add to discussion
-# assuming if we did not observe any attrs, we make no predictions 
-# the baseline doesn't work, and we could just pick randomly one of the attrs
-# but I'm going on the assumption that we wouldn't even know what the attrs are
+fs = []
 
-# create plots
-# x1 = collect(keys(m_b_to_acc))
-# y1 = collect(values(m_b_to_acc))
+nbd_jcs = []
+nbd_dps = []
+nbd_sps = []
 
-# x2 = collect(keys(nbd_b_to_acc))
-# y2 = collect(values(nbd_b_to_acc))
+m_jcs = []
+m_dps = []
+m_sps = []
 
-# x1_sorted, y1_sorted = first.(sort(collect(m_b_to_acc), by=first)), last.(sort(collect(m_b_to_acc), by=first))
-# x2_sorted, y2_sorted = first.(sort(collect(nbd_b_to_acc), by=first)), last.(sort(collect(nbd_b_to_acc), by=first))
+for i in 1:length(nbd_f_to_jc) 
+
+  push!(fs, nbd_f_to_jc[i][1])
+
+  push!(nbd_jcs, nbd_f_to_jc[i][2])
+  push!(nbd_dps, nbd_f_to_dp[i][2])
+  push!(nbd_sps, nbd_f_to_sp[i][2])
+
+  push!(m_jcs, m_f_to_jc[i][2])
+  push!(m_dps, m_f_to_dp[i][2])
+  push!(m_sps, m_f_to_sp[i][2])
+
+
+end
+
+
+
+plot(fs, nbd_jcs, label = "JC", lw=2, color = :red,
+     xlabel = "f - fraction of edges that were observed",
+     ylabel = "Average AUC", 
+     title = "NBD - AUC as a function of f"
+)
+
+plot!(fs, nbd_dps, label = "DP", lw=2, color = :green)
+plot!(fs, nbd_sps, label = "SP", lw =2, color = :blue)
+hline!([.5], label = "AUC = .5", linestyle = :dash, color = :orange)
+savefig(current(), "two-nbd.pdf")
+
+plot(fs, m_jcs, label = "JC", lw=2, color = :red,
+     xlabel = "f - fraction of edges that were observed",
+     ylabel = "Average AUC", 
+     title = "Malaria - AUC as a function of f"
+)
+
+plot!(fs, m_dps, label = "DP", lw=2, color = :green)
+plot!(fs, m_sps, label = "SP", lw =2, color = :blue)
+hline!([.5], label = "AUC = .5", linestyle = :dash, color = :orange)
+savefig(current(), "two-m.pdf")
+
 
 # plot(x1_sorted, y1_sorted, label="Malaria var DBLa Cys-PoLV groups", lw=2, color = :red,
 #     xlabel="β - amount of randomness",
